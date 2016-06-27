b35b8779edb6317ca558780dd52e7f32cf968598
Zack Perdue <zack@ziggidycreative.com>
2016-06-27 15:34:58 +0200

    updated capistrano configs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # Change these
-server '54.164.34.232', port: your_port_num, roles: [:web, :app, :db], primary: true
+server '54.164.34.232', user: 'deploy', roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:ThisIsEpic/TranscoderHeroApp.git'
 set :application,     'transcoder_hero'
@@ -18,10 +18,15 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :rvm_type, :user
+set :rvm_ruby_version, '2.3.0'
 
 ## Defaults:
 # set :scm,           :git
@@ -78,7 +83,3 @@ namespace :deploy do
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
-
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
