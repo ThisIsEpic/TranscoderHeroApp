8813e84dcc5bc8930700f231bcacbd447a5b9e8a
Zack Perdue <zack@ziggidycreative.com>
2016-06-28 16:22:14 +0200

    uncommented code that doesnt exist... oops

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -14,19 +14,19 @@ class Api::V1::JobsController < Api::V1::BaseController
   end
 end
 
-TranscoderHero::Job.create(
-  input: 'https://s3.amazonaws.com/vdb-submit-media-staging/video/d7bf18225e7e92ed963115bae575ec26a2420e33.mov',
-  profiles: ['Virtual Diamond Resize', 'Virtual Diamond Crop', 'Extract Frames'],
-  override: {
-    crop: {
-      w: 1080,
-      h: 1080,
-      left: 420,
-      top: 0
-    },
-    scale: {
-      width: 600,
-      height: 600
-    }
-  }
-)
+# TranscoderHero::Job.create(
+#   input: 'https://s3.amazonaws.com/vdb-submit-media-staging/video/d7bf18225e7e92ed963115bae575ec26a2420e33.mov',
+#   profiles: ['Virtual Diamond Resize', 'Virtual Diamond Crop', 'Extract Frames'],
+#   override: {
+#     crop: {
+#       w: 1080,
+#       h: 1080,
+#       left: 420,
+#       top: 0
+#     },
+#     scale: {
+#       width: 600,
+#       height: 600
+#     }
+#   }
+# )
