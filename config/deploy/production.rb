8028b6326576a0357a30e54194a603ac4ef098d7
Zack Perdue <zack@ziggidycreative.com>
2016-06-28 12:22:54 +0200

    removed config secrets and added it to linked files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -36,7 +36,7 @@ set :rvm_ruby_version, '2.3.0'
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
