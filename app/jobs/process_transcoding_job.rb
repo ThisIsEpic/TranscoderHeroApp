7e00656613e58bbecf813db28ac8fc2fe20f8084
Zack Perdue <zack@ziggidycreative.com>
2016-07-02 01:00:45 -0400

    dont process jobs that dont exist

--- app/jobs/process_transcoding_job.rb
+++ app/jobs/process_transcoding_job.rb
@@ -2,6 +2,7 @@ class ProcessTranscodingJob < ApplicationJob
   queue_as :default
 
   def perform(job)
+    return true unless job
     TranscodingJobManager.new(job).process! if job.created?
   end
 end
